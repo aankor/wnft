38f639fb985d7eaebc55b8dbaeb97a302b05d7d8
aankor <aankor@marinade.finance>
2023-08-22 12:34:01 +0200

    Add network label

--- packages/wnft-app/src/components/WnftAppBar.tsx
+++ packages/wnft-app/src/components/WnftAppBar.tsx
@@ -1,13 +1,17 @@
+import { Stack } from '@mui/joy';
 import {AppBar, Toolbar, Typography} from '@mui/material';
 import {WalletMultiButton} from '@solana/wallet-adapter-material-ui';
 import {FC} from 'react';
 
 const WnftAppBar: FC = () => {
   return (
-    <AppBar position='fixed'>
+    <AppBar position="fixed">
       <Toolbar>
         <Typography>Wallet NFT</Typography>
-        <WalletMultiButton color="secondary" sx={{marginLeft: 'auto'}} />
+        <Stack direction='row' sx={{marginLeft: 'auto'}}>
+          <Typography sx={{paddingTop: '0.3em', marginRight: '1em'}}>Network: Devnet</Typography>
+          <WalletMultiButton color="secondary" sx={{marginLeft: 'auto'}} />
+        </Stack>
       </Toolbar>
     </AppBar>
   );
